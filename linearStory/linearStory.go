29323133a049eea950ce5eba6cca94016d96994c
dyanneo <dyanneo@yahoo.com>
2019-07-05 18:57:37 -0500

    notes on functions

--- linearStory/linearStory.go
+++ linearStory/linearStory.go
@@ -2,28 +2,29 @@ package main
 
 import "fmt"
 
-// this is a linked list
+// this is a doubly linked list (each page has a next and prev)
 type storyPage struct {
 	text     string
 	nextPage *storyPage
 	prevPage *storyPage
 }
 
-/*
+// receiver syntax for putting methods on structs
+// go does not do tail call elimination, which means the stack
+// will keep growing, rather than turn the last statement into a loop
+// automatically, the way some compilers will do
 func (page *storyPage) playStory() {
-	if page == nil {
-		return
-	}
-	fmt.Println(page.text)
-	page.nextPage.playStory()
-}
-*/
-func (page *storyPage) playStory() {
-	for page != nil {
+	if page != nil {
 		fmt.Println(page.text)
 		page = page.nextPage
+
 	}
 }
+
+// function = has a return value
+// procedure = executes commands with no return value
+// method = functions attached to a struct/object
+
 func main() {
 	page1 := storyPage{"It was a dark and stormy night", nil, nil}
 	page2 := storyPage{"You are alone and have to find the sacred helmet before the bad guys do", nil, &page1}
@@ -43,4 +44,8 @@ func addPage(currentPage *storyPage, newPage *storyPage) {
 	if newPage.nextPage != nil {
 		newPage.nextPage.prevPage = newPage
 	}
+}
+
+func deletePage() {
+
 }
