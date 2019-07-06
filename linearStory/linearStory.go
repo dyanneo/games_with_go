788e3fc5cf238aff60770762bcaea6e7617cf735
dyanneo <dyanneo@yahoo.com>
2019-07-05 19:19:58 -0500

    adds add after function

--- linearStory/linearStory.go
+++ linearStory/linearStory.go
@@ -6,7 +6,7 @@ import "fmt"
 type storyPage struct {
 	text     string
 	nextPage *storyPage
-	prevPage *storyPage
+	//prevPage *storyPage
 }
 
 // receiver syntax for putting methods on structs
@@ -14,11 +14,23 @@ type storyPage struct {
 // will keep growing, rather than turn the last statement into a loop
 // automatically, the way some compilers will do
 func (page *storyPage) playStory() {
-	if page != nil {
+	for page != nil {
 		fmt.Println(page.text)
 		page = page.nextPage
 
 	}
+}
+
+func (page *storyPage) addToEnd(text string) {
+	for page.nextPage != nil {
+		page = page.nextPage
+	}
+	page.nextPage = &storyPage{text, nil}
+}
+
+func (page *storyPage) addAfter(text string) {
+	newPage := &storyPage{text, page.nextPage}
+	page.nextPage = newPage
 }
 
 // function = has a return value
@@ -26,26 +38,25 @@ func (page *storyPage) playStory() {
 // method = functions attached to a struct/object
 
 func main() {
-	page1 := storyPage{"It was a dark and stormy night", nil, nil}
-	page2 := storyPage{"You are alone and have to find the sacred helmet before the bad guys do", nil, &page1}
-	page3 := storyPage{"You see a troll ahead", nil, &page2}
-	page1.nextPage = &page2
-	page2.nextPage = &page3
+	page1 := storyPage{"You are standing in an open field west of a white house.", nil}
+	page1.addToEnd("You climb into the attic, it is pitch black, you can't see a thing")
+	page1.addToEnd("You are eaten by a Grue.")
 
+	page1.addAfter("testing add after")
 	page1.playStory() //may be refered to as a method call
 
 }
 
-func addPage(currentPage *storyPage, newPage *storyPage) {
-	followingPage := currentPage.nextPage
-	currentPage.nextPage = newPage
-	newPage.nextPage = followingPage
-	newPage.prevPage = currentPage
-	if newPage.nextPage != nil {
-		newPage.nextPage.prevPage = newPage
-	}
-}
+// func addPage(currentPage *storyPage, newPage *storyPage) {
+// 	followingPage := currentPage.nextPage
+// 	currentPage.nextPage = newPage
+// 	newPage.nextPage = followingPage
+// 	newPage.prevPage = currentPage
+// 	if newPage.nextPage != nil {
+// 		newPage.nextPage.prevPage = newPage
+// 	}
+// }
 
-func deletePage() {
+// func deletePage() {
 
-}
+// }
