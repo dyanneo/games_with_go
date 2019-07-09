f2d1a793a497d0f947c69e0bf91fa2bb5f366cf8
dyanneo <dyanneo@yahoo.com>
2019-07-08 20:51:57 -0500

    wrap function to finish loop

--- branchingStory/branchingStory.go
+++ branchingStory/branchingStory.go
@@ -1,11 +1,48 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
 type storyNode struct {
 	text    string
 	yesPath *storyNode
 	noPath  *storyNode
 }
 
-func main() {
+func (node *storyNode) play() {
+	fmt.Println(node.text)
 
+	if node.yesPath != nil && node.noPath != nil {
+
+		scanner := bufio.NewScanner(os.Stdin)
+
+		for {
+			scanner.Scan()
+			answer := scanner.Text()
+
+			if answer == "yes" {
+				node.yesPath.play()
+				break
+
+			} else if answer == "no" {
+				node.noPath.play()
+				break
+			} else {
+				fmt.Println("That is not a valid answer. Please answer yes or no")
+			}
+		}
+	}
+}
+
+func main() {
+	root := storyNode{"You are at the entrance to a cave. Do you want to go in the cave?", nil, nil}
+	winning := storyNode{"You win", nil, nil}
+	losing := storyNode{"You lose", nil, nil}
+	root.yesPath = &losing
+	root.noPath = &winning
+
+	root.play()
 }
